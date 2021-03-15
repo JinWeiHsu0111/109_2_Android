00b8a1b1fc1001cd2a4ee0a085a4abdd0c137574
JinWeiHsu0111 <79504890+JinWeiHsu0111@users.noreply.github.com>
2021-03-15 18:01:19 +0800

    Add files via upload

---
+++ 108590034_HW3_1/app/src/main/java/com/example/a108590034_hw3_1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a108590034_hw3_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
